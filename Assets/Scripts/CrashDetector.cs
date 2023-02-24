ffe2d0d5d99f778483f7a67fffd2e28def2cbe70
DESKTOP-N5R9FT2\Fritz <fritzjohn_solibio@yahoo.ca>
2023-02-23 23:51:28 -0800

    hooked particle effects to the code

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,10 +4,13 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float crashDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag.Equals("Ground"))
         {
+            crashEffect.Play();
             Invoke("ReloadScene", crashDelay);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float finishDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag.Equals("Player"))
         {
+            finishEffect.Play();
             Invoke("ReloadScene", finishDelay);
         }
     }
